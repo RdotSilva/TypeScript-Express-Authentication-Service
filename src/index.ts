be980a8c2d5a2bebe82aee2acb5072c172d227c0
RdotSilva <RyandotSilva@gmail.com>
2020-09-07 11:37:43 -0400

    Use router in main express app

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import express, { Response, Request } from "express";
+import { router } from "./routes/loginRoutes";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(`
-  <div>
-    <h1>Hi There</h1>
-  </div>`);
-});
+app.use(router);
 
 app.listen(3000, () => {
   console.log(`Listening on port 3000`);
