e869ab24bd84b9c9fec0886815551b4ab41802ee
RdotSilva <RyandotSilva@gmail.com>
2020-09-07 11:33:50 -0400

    Set up express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("App Started");
+import express, { Response, Request } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`
+  <div>
+    <h1>Hi There</h1>
+  </div>`);
+});
+
+app.listen(3000, () => {
+  console.log(`Listening on port 3000`);
+});
