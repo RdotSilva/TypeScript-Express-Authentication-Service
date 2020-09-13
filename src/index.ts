7a6422d5e28db861a7b9ddfb1a8533e421f22ed8
RdotSilva <RyandotSilva@gmail.com>
2020-09-13 19:55:17 -0400

    Add controller router to main app

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express, { Response, Request } from "express";
 import { router } from "./routes/loginRoutes";
 import cookieSession from "cookie-session";
+import "./controllers/LoginController";
+import { router as controllerRouter } from "./controllers/decorators/controller";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["secretKey"] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log(`Listening on port 3000`);
