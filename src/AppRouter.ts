1ebf91fe592b152eee605832b90d67a5a04874f3
RdotSilva <RyandotSilva@gmail.com>
2020-09-14 20:23:44 -0400

    Add getInstance method

--- src/AppRouter.ts
+++ src/AppRouter.ts
@@ -2,4 +2,18 @@ import express from "express";
 
 export class AppRouter {
   private static instance: express.Router;
+
+  /**
+   * Create a singleton Express Router
+   * @static
+   * @returns {express.Router} - Express Router
+   * @memberof AppRouter
+   */
+  static getInstance(): express.Router {
+    if (!AppRouter.instance) {
+      AppRouter.instance = express.Router();
+    }
+
+    return AppRouter.instance;
+  }
 }
