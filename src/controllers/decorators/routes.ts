81cbb3466ba50b270ac7519c53c40235cc91fa38
RdotSilva <RyandotSilva@gmail.com>
2020-09-14 20:36:40 -0400

    Add routeBinder method

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,15 +1,15 @@
 import "reflect-metadata";
 
-export function get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata("path", path, target, key);
-    Reflect.defineMetadata("method", "get", target, key);
-  };
-}
-
-export function post(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata("path", path, target, key);
-    Reflect.defineMetadata("method", "post", target, key);
+/**
+ * Binds a http method to a path
+ * @param {string} method - Get, Put, Post, Delete, Patch
+ * @returns
+ */
+function routeBinder(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata("path", path, target, key);
+      Reflect.defineMetadata("method", method, target, key);
+    };
   };
 }
