534056378424996e120315cbf86508665f1bfa4b
RdotSilva <RyandotSilva@gmail.com>
2020-09-14 20:31:45 -0400

    Update main app to use singleton router

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import express, { Response, Request } from "express";
+import express from "express";
 import { router } from "./routes/loginRoutes";
 import cookieSession from "cookie-session";
 import "./controllers/LoginController";
-import { router as controllerRouter } from "./controllers/decorators/controller";
+import { AppRouter } from "./AppRouter";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["secretKey"] }));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log(`Listening on port 3000`);
