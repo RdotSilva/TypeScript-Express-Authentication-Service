fbd7804284a8488e563230d047f536c4d9706189
RdotSilva <RyandotSilva@gmail.com>
2020-09-16 18:06:49 -0400

    Add middleware metadata

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -19,9 +19,11 @@ export function controller(routePrefix: string) {
         target.prototype,
         key
       );
+      const middlewares =
+        Reflect.getMetadata(MetadataKeys.Middleware, target, key) || [];
 
       if (path) {
-        router[method](`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
       }
     }
   };
