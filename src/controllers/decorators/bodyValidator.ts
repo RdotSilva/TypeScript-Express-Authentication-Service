98583bc29d00c3e55587d6c250983eb290bf6aad
RdotSilva <RyandotSilva@gmail.com>
2020-09-18 20:04:16 -0400

    Add bodyValidator decorator method

--- src/controllers/decorators/bodyValidator.ts
+++ src/controllers/decorators/bodyValidator.ts
@@ -1,1 +1,8 @@
-// TODO: Implement body validator decorator
+import "reflect-metadata";
+import { MetadataKeys } from "./MetadataKeys";
+
+export function bodyValidator(...keys: string[]) {
+  return function (target: any, key: string, desc: PropertyDescriptor) {
+    Reflect.defineMetadata(MetadataKeys.Validator, keys, target, key);
+  };
+}
