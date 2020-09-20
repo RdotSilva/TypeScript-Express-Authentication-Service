ef4f19778b84fcf3a772a56751271562dd98d359
RdotSilva <RyandotSilva@gmail.com>
2020-09-19 20:47:48 -0400

    Add getRoot method

--- src/controllers/RootController.ts
+++ src/controllers/RootController.ts
@@ -1,3 +1,22 @@
 import { Request, Response } from "express";
-import { get } from "./decorators";
-class RootController {}
+import { get, controller } from "./decorators";
+
+@controller("")
+class RootController {
+  @get("/")
+  getRoot(req: Request, res: Response) {
+    if (req.session && req.session.loggedIn) {
+      res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/auth/logout">Logout</a>
+      </div>`);
+    } else {
+      res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/auth/login">Login</a>
+      </div>`);
+    }
+  }
+}
